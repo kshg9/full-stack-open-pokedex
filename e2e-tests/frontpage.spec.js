33bcfbf54d88af5d9c8e179d708d176d159c70cb
ion <142713268+kshg9@users.noreply.github.com>
2025-07-10 00:21:16 +0530

    re-test

--- e2e-tests/frontpage.spec.js
+++ e2e-tests/frontpage.spec.js
@@ -1,11 +1,9 @@
-const { test, expect, describe } = require('@playwright/test')
+const { test, describe, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
-    await page.goto('http://localhost:5000')
+    await page.goto('')
     await expect(page.getByText('ivysaur')).toBeVisible()
-    await expect(
-      page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')
-    ).toBeVisible()
+    await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 })
