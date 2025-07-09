9ac8bcdab834e18c40fbc9946810dd2b73d9a6e4
ion <142713268+kshg9@users.noreply.github.com>
2025-07-10 00:47:04 +0530

    re-test again

--- playwright.config.js
+++ playwright.config.js
@@ -73,8 +73,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000',
+    command: 'npm run start-prod',
+    url: 'http://localhost:5000',
     reuseExistingServer: !process.env.CI,
   },
 })
