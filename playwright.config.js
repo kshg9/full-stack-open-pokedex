eb83dd54def3a10fa9a736aa3c279b9c48f509d5
ion <142713268+kshg9@users.noreply.github.com>
2025-07-10 00:05:14 +0530

    re-test

--- playwright.config.js
+++ playwright.config.js
@@ -3,10 +3,4 @@ const { defineConfig } = require('@playwright/test')
 
 module.exports = defineConfig({
   testDir: './e2e-tests',
-  webServer: {
-    command: 'npm start-prod',
-    port: 5000,
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
 })
